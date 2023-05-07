77baa629489141e0600f84267c5cd28e071d9669
Ethan Hancock <ehancock1648@gmail.com>
2023-05-06 21:51:38 -0500

    fix viewData

--- frontend/src/routes/ViewData.js
+++ frontend/src/routes/ViewData.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles/ViewData.css";
+import Pagination from "../components/Pagination";
 import Product from "../components/Product";
 import { addToCart } from "../reducers/cartSlice";
 import { setProductData } from "../reducers/dataSlice";
@@ -58,26 +59,11 @@ const ViewData = () => {
     const reset = () => {
         dispatch(setProductData(originalData));
     };
-
-    // const sortPriceHigh = () => {
-    //     let newData = originalData;
-    //     dispatch(setProductData(newData.sort((a, b) => {
-    //         return a.selling_price - b.selling_price;
-    //     })));
-    // };
-    // const sortPriceLow = () => {
-    //     let newData = originalData;
-    //     dispatch(setProductData(newData.sort((a, b) => {
-    //         return b.selling_price - a.selling_price;
-    //     })));
-    // };
     
     return (
         <div className="container productView">
-            <div class="px-4 py-5 pb-0 text-center">
-                <h1 class="display-5 fw-bold">Featured Products</h1>
-            </div>
-
+            {!isLoading ? 
+            <>
             <div id="myCarousel" className="carousel slide pointer-event" data-bs-ride="carousel">
                 <div className="carousel-indicators">
                     <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="" aria-label="Slide 1"></button>
@@ -88,17 +74,17 @@ const ViewData = () => {
                 <div className="carousel-inner">
                     <div className="carousel-item">
                         <div className="row row-cols-2">
-                            <Product {...productData[0]}/>
+                            <Product {...productData[feat1]} isFeatured={1}/>
                         </div>
                     </div>
                     <div className="carousel-item active">
                         <div className="row row-cols-2">
-                            <Product {...productData[1]}/>
+                            <Product {...productData[feat2]} isFeatured={1}/>
                         </div>
                     </div>
                     <div className="carousel-item">
                         <div className="row row-cols-2">
-                            <Product {...productData[2]}/>
+                            <Product {...productData[feat3]} isFeatured={1}/>
                         </div>
                     </div>
                 </div>
@@ -116,7 +102,7 @@ const ViewData = () => {
             <div className="row row-cols-2">
                 <div className="col-sm-2">
                     <div className="card">
-                        <div className="card-header fs-5 fw-bold">
+                        <div className="card-header fs-5">
                             Categories
                         </div>
                         <ul className="list-group list-group-flush">
@@ -155,32 +141,36 @@ const ViewData = () => {
                             </button>
                         </ul>
                     </div>
-
-                    {/* <div class="dropdown my-3">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Sort By Price
-                        </button>
-                        <ul class="dropdown-menu">
-                            <button className="list-group-item list-group-item-action" onClick={() => sortPriceHigh()}>
-                                Highest First
-                            </button>
-                            <button className="list-group-item list-group-item-action" onClick={() => sortPriceLow()}>
-                                Lowest First
-                            </button>
-                        </ul>
-                    </div> */}
                 </div>
-                
+
                 <div className="col-lg-10">
-                    <div className="row row-cols-3">
-                        {productData.map((element) => {
+                    <div className="fs-4">
+                        {(productData.length).toLocaleString()} Results...
+                    </div>
+                    <div key={currentPageData.length} className="row row-cols-3">
+                        {currentPageData.map((element) => {
                             return(
-                                <Product {...element}/>
+                                <Product {...element} isFeatured={0}/>
                             )
                         })}
                     </div>
+                    <div className="d-flex justify-content-center mt-5">
+                        <Pagination
+                            className="pagination-bar"
+                            currentPage={currentPage}
+                            totalCount={productData.length}
+                            pageSize={PageSize}
+                            onPageChange={page => setCurrentPage(page)}
+                        />
+                    </div>
                 </div>
             </div>
+            </> : 
+            <div class="d-flex justify-content-center  fs-3 m-5">
+                <div class="spinner-border spin fs-1" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            </div>}
         </div>
     )
 }
